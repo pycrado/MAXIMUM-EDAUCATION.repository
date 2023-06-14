604c34808638c2362f516e92e27833bebcddbb92
pycrado <rinazaglamov2007@gmail.com>
2023-06-14 14:39:54 +0400

    Beta

---
+++ main.py
@@ -0,0 +1,124 @@
+# # def strcounter(s):
+# #     for lit in s:
+# #         counter = 0
+# #         for sub_lit in s:
+# #             if lit == sub_lit:
+# #                 counter +=1
+# #             print(lit,counter)
+# #
+# # strcounter('abcd')
+# #
+# #
+# # str = 'eqeeq123121oidtgfrhjmlaerkhgzdfjghdghldjgzghghzkwqwr3klki14230322131q8qeqffweq'
+# # print(str)
+# # print(set(str))
+# #
+# #
+# def strcounter(s):
+#     for lit in set(s):
+#         counter = 0
+#         for sub_lit in s:
+#             if lit == sub_lit:
+#                 counter +=1
+#             print(lit, counter)
+#
+# strcounter("sa")
+#
+#
+#
+#
+# def strcounter(s):
+#     lits_counter = {}
+#     for lit in s:
+#         lits_counter[lit] = lits_counter.get(lit,0) + 1
+#     for lits, conter in lits_counter.items():
+#         print(lits,conter)
+#
+# strcounter("213addsadsggsdsf")
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
